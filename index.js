5c2e649541ad686236217f964a668a2cef931fb4
Sebastian Diaz Monfort <diazmonfortsebastian@gmail.com>
2023-11-23 13:07:41 -0300

    prueba validate

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-console.log('hola')
+var validator = require("validator");
+
+async function validate(){
+    const isEmail = await validator.isEmail("ejemplo@ejemplo.com");
+    if (isEmail) {
+        console.log("Es un email");
+    } else {
+        console.log("No es un email")
+    }
+}
+
+validate()
